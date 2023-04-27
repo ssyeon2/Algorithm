5f825501a15f7ce0caadea0b107577d4d836ec10
Sen <105052724+ssyeon2@users.noreply.github.com>
2023-04-27 17:40:11 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,5 @@
 N = int(input())
 score = list(map(int, input().split(' ')))
-
 maxi = max(score)
-
-mean = 0
-for i in score :
-    mean += (i / maxi) * 100
-print(mean/N)
+total = sum(score)
+print(total * 100 / maxi /N)

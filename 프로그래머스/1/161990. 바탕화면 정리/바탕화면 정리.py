c8605387216cd6fc5443b1d2be4bc28de44b23b5
Sen <105052724+ssyeon2@users.noreply.github.com>
2023-10-18 18:38:44 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,17 +1,11 @@
 # x와 y의 최대, 최소 도출하기
 def solution(wallpaper):
-    x_min, y_min, x_max, y_max = len(wallpaper), len(wallpaper[0]), 0, 0
+    all_row = []
+    all_col = []
     for row in range(len(wallpaper)):
         for col in range(len(wallpaper[0])):
             if wallpaper[row][col] == '#':
-                print(row, col)
-                if row <= x_min :
-                    x_min = row
-                if row > x_max :
-                    x_max = row
-                if col <= y_min :
-                    y_min = col
-                if col > y_max :
-                    y_max = col
-    answer = [x_min, y_min, x_max+1, y_max+1]
+                all_row.append(row)
+                all_col.append(col)
+    answer = [min(all_row), min(all_col), max(all_row)+1, max(all_col)+1]
     return answer
